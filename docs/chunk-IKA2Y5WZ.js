c10874ec3cca41935faf01c4c06fff1bd24bfea0
Gabriela25 <gabrielabarreto25@gmail.com>
2024-05-31 16:25:08 -0500

    maps ratios3

---
+++ docs/chunk-IKA2Y5WZ.js
@@ -0,0 +1,1 @@
+import"./chunk-TMC7WMLO.js";var o=[{path:"",loadComponent:()=>import("./chunk-2WR4G5ZB.js")}],t=o;export{t as default,o as routes};
---
+++ docs/server/chunk-NB2LAUBK.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import"./chunk-VVCT4QZE.mjs";var o=[{path:"",loadComponent:()=>import("./chunk-4Q6KSRA4.mjs")}],t=o;export{t as default,o as routes};
